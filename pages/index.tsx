e162e4108c54f8f4d427c94d7c6897ea1bede353
AtharvaCM <necrons999@gmail.com>
2022-10-20 23:24:06 +0530

    file structure changed and button component added

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,9 +2,15 @@ import type { NextPage } from "next";
 
 // custom components
 import Layout from "@/components/layout";
+import Button from "@/components/UI/button";
 
 const Home: NextPage = () => {
-  return <Layout>Home Page</Layout>;
+  return (
+    <Layout>
+      <p>Home Page</p>
+      <Button variant="primary" />
+    </Layout>
+  );
 };
 
 export default Home;
