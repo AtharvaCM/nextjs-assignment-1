73e948e55780087e2e1cebe6dcd3651fd0c18124
AtharvaCM <necrons999@gmail.com>
2022-10-21 16:26:14 +0530

    footer modified

--- src/components/layout/footer/index.tsx
+++ src/components/layout/footer/index.tsx
@@ -1,7 +1,39 @@
 import React from "react";
 
-const Footer = () => {
-  return <div className="bg-slate-300 px-6 py-8 text-gray-800">Footer</div>;
+const Footer: React.FC = () => {
+  return (
+    <footer className="bg-white p-4 shadow dark:bg-gray-800 md:flex md:items-center md:justify-between md:p-6">
+      <span className="text-sm text-gray-500 dark:text-gray-400 sm:text-center">
+        © 2022{" "}
+        <a href="https://atharvacm.netlify.app/" className="hover:underline">
+          AtharvaCM
+        </a>
+        . All Rights Reserved.
+      </span>
+      <ul className="mt-3 flex flex-wrap items-center text-sm text-gray-500 dark:text-gray-400 sm:mt-0">
+        <li>
+          <a href="#" className="mr-4 hover:underline md:mr-6 ">
+            About
+          </a>
+        </li>
+        <li>
+          <a href="#" className="mr-4 hover:underline md:mr-6">
+            Privacy Policy
+          </a>
+        </li>
+        <li>
+          <a href="#" className="mr-4 hover:underline md:mr-6">
+            Licensing
+          </a>
+        </li>
+        <li>
+          <a href="#" className="hover:underline">
+            Contact
+          </a>
+        </li>
+      </ul>
+    </footer>
+  );
 };
 
 export default Footer;
