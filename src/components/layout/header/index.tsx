8e9fb727e07321cfc5c01ac7a89ff9a907e5b89b
AtharvaCM <necrons999@gmail.com>
2022-10-21 16:26:28 +0530

    header bgcolor change

--- src/components/layout/header/index.tsx
+++ src/components/layout/header/index.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Header = () => {
   return (
     <header className="sticky top-0 z-[1000]">
-      <nav className="bg-slate-400 p-4 text-white font-bold">
+      <nav className="bg-slate-600 p-4 font-bold text-white">
         NextJS Assignment 1
       </nav>
     </header>
